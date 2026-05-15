ace3a7c9f4b20699b5c5bf8c43e095bc5b89ea3f
usany <ckd_qja@naver.com>
2026-05-15 19:46:47 +0900

    cern

--- pages/views.py
+++ pages/views.py
@@ -5,23 +5,23 @@ from .models import MenuItem
 
 def home(request):
     """Home page"""
-    return render(request, 'menu/home.html')
+    return render(request, 'pages/home.html')
 
 
 def menu_list(request):
     """Display all menu items as a tree structure"""
     root_items = MenuItem.objects.filter(parent=None)
-    return render(request, 'menu/menu_list.html', {'menu_items': root_items})
+    return render(request, 'pages/menu_list.html', {'menu_items': root_items})
 
 
 def menu_detail(request, pk):
     """Display details for a specific menu item"""
     menu_item = MenuItem.objects.get(pk=pk)
-    return render(request, 'menu/menu_detail.html', {'menu_item': menu_item})
+    return render(request, 'pages/menu_detail.html', {'menu_item': menu_item})
 
 
 # @staff_member_required
 def admin_view(request):
     """Custom admin view for managing menu items"""
     items = MenuItem.objects.all()
-    return render(request, 'menu/admin_view.html', {'items': items})
+    return render(request, 'pages/admin_view.html', {'items': items})
--- restaurants/urls.py
+++ restaurants/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from menu import views
+# from menu import views
+from pages import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', views.admin_view, name='admin_view'),
     path('django-admin/', admin.site.urls),
-    path('menu/', include('menu.urls')),
+    path('menu/', include('pages.urls')),
 ]
