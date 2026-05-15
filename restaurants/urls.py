e5a938c36a14bc19ccbda8eca0fdcdfaa93a440e
usany <ckd_qja@naver.com>
2026-05-15 20:13:51 +0900

    cern

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-# from menu import views
 from pages import views
 
 urlpatterns = [
