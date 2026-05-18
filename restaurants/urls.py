d77bfa50c89d9289e43e1c5675aa4deda0e3ebde
charlie <ckd_qja@naver.com>
2026-05-19 01:03:06 +0900

    cern

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from pages import views
+from . import views
 
 urlpatterns = [
     path('', views.root_redirect, name='root'),
