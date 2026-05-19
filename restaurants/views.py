d4e69ebb4027624721bff2e2e1f8cb361d990ad5
usany <ckd_qja@naver.com>
2026-05-19 17:34:15 +0900

    cern

--- restaurants/views.py
+++ restaurants/views.py
@@ -42,7 +42,7 @@ def home(request):
     menu_items = [
         {'id': 1, 'title': '푸른솔 학생식당', 'path': 'ph', 'mealsSemester': ['아침', '점심'], 'mealsVacation': ['아침', '점심']},
         {'id': 2, 'title': '푸른솔 교직원식당', 'path': 'pg', 'mealsSemester': ['점심'], 'mealsVacation': ['점심']},
-        {'id': 3, 'title': '청운관 학생식당', 'path': 'ch', 'mealsSemester': ['아침', '점심', '저녁'], 'mealsVacation': ['아침', '점심', '저녁']},
+        {'id': 3, 'title': '청운관 학생식당', 'path': 'ch', 'mealsSemester': ['아침', '점심', '저녁'], 'mealsVacation': ['점심']},
         {'id': 4, 'title': '청운관 교직원식당', 'path': 'cg', 'mealsSemester': ['점심'], 'mealsVacation': ['점심']},
         {'id': 5, 'title': '한국외대 인문관 식당', 'path': 'hi', 'mealsSemester': ['아침', '점심', '저녁'], 'mealsVacation': ['아침', '점심', '저녁']},
         {'id': 6, 'title': '한국외대 교수회관 식당', 'path': 'hg', 'mealsSemester': ['아침', '점심', '저녁'], 'mealsVacation': ['아침', '점심', '저녁']},
