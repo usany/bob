c49e635a0a2edcbd028d5a36d96344f511f6f025
usany <ckd_qja@naver.com>
2026-05-26 02:26:15 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -136,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'pages' / 'static',
+]
 
 # LOGIN_URL = '/django-admin/login/'
 LOGOUT_REDIRECT_URL = 'home'
