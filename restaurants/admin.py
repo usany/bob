c02e6f556ae35d94dcbc9b4b99db2c5b962a64be
usany <ckd_qja@naver.com>
2026-05-26 06:23:14 +0900

    cern

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -3,7 +3,7 @@ from restaurants.models import MenuItem
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'main', 'side', 'price', 'order', 'pork')
-    list_filter = ('pork', 'main', 'side')
+    list_display = ('title', 'main', 'side', 'price', 'order', 'non_pork')
+    list_filter = ('non_pork', 'main', 'side')
     search_fields = ('title', 'main', 'side', 'place')
     ordering = ('order',)
