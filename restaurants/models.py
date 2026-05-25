209efd72e0893e6d9e0eff515279eb72eb145927
usany <ckd_qja@naver.com>
2026-05-26 04:09:01 +0900

    cern

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,6 +5,7 @@ class MenuItem(models.Model):
     # title = models.CharField(max_length=200)
     # order = models.IntegerField(default=0)
     # parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    # parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     main = models.CharField(max_length=200)
     side = models.CharField(max_length=200)
     time = models.CharField(max_length=200)
