205dd818f5bcd20c7a973e5fbb4f8d3c4559938c
usany <ckd_qja@naver.com>
2026-05-26 04:08:47 +0900

    cern

--- restaurants/models.py
+++ restaurants/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class MenuItem(models.Model):
-    title = models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
-    order = models.IntegerField(default=0)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    # title = models.CharField(max_length=200)
+    # order = models.IntegerField(default=0)
+    # parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     main = models.CharField(max_length=200)
     side = models.CharField(max_length=200)
     time = models.CharField(max_length=200)
     place = models.CharField(max_length=200)
     extra = models.CharField(max_length=200)
+    day = models.CharField(max_length=200)
     price = models.IntegerField(default=0)
     pork = models.BooleanField(default=False)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    url = models.CharField(max_length=200)
 
     class Meta:
         ordering = ['order']
