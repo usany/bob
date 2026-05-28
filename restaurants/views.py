b19599727861aefb00b1c9eb7f953d015c0be9cd
charlie <ckd_qja@naver.com>
2026-05-28 15:19:40 +0900

    cern

--- restaurants/views.py
+++ restaurants/views.py
@@ -213,6 +213,19 @@ FIXED_MENU = {
         "storage_url": ''
     },
     {
+        "main": "토핑대파쏭쏭",
+        "side": None,
+        "price": 500,
+        "day": None,
+        "time_category": ["self"],
+        "time_detail": None,
+        "place": "ch",
+        "extra_menu": None,
+        "extra_price": None,
+        "non_pork": False,
+        "storage_url": ''
+    },
+    {
         "main": "음료(콜라,사이다)",
         "side": None,
         "price": 1700,
