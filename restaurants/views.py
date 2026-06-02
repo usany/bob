c5377bd001903f0120aff78c5761ecba5f1b20cc
charlie <ckd_qja@naver.com>
2026-06-03 02:09:45 +0900

    cern

--- restaurants/views.py
+++ restaurants/views.py
@@ -178,10 +178,6 @@ def menu_list(request, path, meal=None, base=None, bases=None):
         if selected_meal in d.get('time_category', [])
     ] if selected_meal else all_dishes
     db_qs = MenuItem.objects.filter(place=path, time=selected_meal, day=selected_day)
-    # if selected_meal:
-    #     db_qs = db_qs.filter(time=selected_meal)
-    # if selected_day:
-    #     db_qs = db_qs.filter(day=selected_day)
     filtered_dishes = filtered_dishes + list(db_qs)
 
 
