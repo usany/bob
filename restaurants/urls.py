94057afae91e089d226ee4ce70ee9a231aff9dea
usany <ckd_qja@naver.com>
2026-06-02 16:40:08 +0900

    Remove __pycache__ from git tracking and update .gitignore

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -21,7 +21,6 @@ from . import views
 
 urlpatterns = [
     path('', views.root_redirect, name='root'),
-    # path('<str:base>/<str:bases>/', views.home_menu, name='home'),
     path('se/', views.home, name='home'),
     path('se/<str:path>/', views.menu_list, name='menu_list_se'),
     path('se/<str:path>/<str:meal>/', views.menu_detail, name='menu_detail_se'),
@@ -32,4 +31,6 @@ urlpatterns = [
     # path('adminpage/', views.admin_view, name='admin_view'),
     # path('pages/', include('pages.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    # Generic language/campus route - MUST BE LAST to avoid catching other routes
+    path('<str:base>/<str:bases>/', views.home_menu, name='home_menu'),
 ]
